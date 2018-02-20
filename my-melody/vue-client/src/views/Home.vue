9405cf9e3fa713ac49ba87d0ae52752a5a5f20bc
Marlo Delatorre <delatorm@purdue.edu>
2018-02-20 03:32:20 -0500

    re-add vue-client

---
+++ my-melody/vue-client/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <LandingCard/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingCard from '@/components/LandingCard.vue'
+export default {
+  name: 'home',
+  components: {
+    LandingCard
+  }
+}
+</script>
