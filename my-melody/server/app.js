dea1d8c340727b1c3ab4e9d6c723c316da337645
Pranav Shekar <pshekar@purdue.edu>
2018-02-22 03:02:09 -0500

    should have server working now

--- my-melody/server/app.js
+++ my-melody/server/app.js
@@ -19,7 +19,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/dist')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/dist/', '/index.html'));
+});
 
 // Route Registration
 app.use('/api/users', users);
