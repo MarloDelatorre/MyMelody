d4014199ab11987f7ea1a5b4d3a2b8fa49437be3
Vincent Jiang <jiangv.purdue.edu>
2018-02-23 01:55:10 -0500

    Working on login page styling, added messages folder client-side

---
+++ my-melody/client/src/components/NavBarLogin.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="navBarLoginContainer">
+        <header class="projectLogo">{{ projectNameMessage }}</header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBarLogin',
+        data: function() {
+            return {
+                projectNameMessage: 'Vibe'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .navBarLoginContainer {
+        width: 100%;
+        color: #FFFFFF;
+        height: 120px;
+        text-align: center;
+    }
+
+    .projectLogo {
+        font-size: 60px;
+        vertical-align: middle;
+        line-height: 120px;
+        margin-left: 50px;
+        font-weight: 600;
+        color: #D34084;
+    }
+</style>
--- my-melody/client/src/views/Login.vue
+++ my-melody/client/src/views/Login.vue
@@ -1,27 +1,39 @@
 <template>
-<div id="full">
-  <div>
-    <input type="text" v-model="message" placeholder="Enter email"/>
-  </div>
-  <div>
-    <input type="password" v-model="password" placeholder="Password"/>
-  </div>
-  <div>
-    <button v-on:click="login" class="login">Login</button>
-  </div>
-</div>
+    <div>
+        <!-- <navBarLogin/> -->
+        <div id="full">
+            <div class="projectNameContainer">
+                <header class="projectName">{{ loginMessage.projectName }}</header>
+            </div>
+              <div>
+                  <input type="text" v-model="message" placeholder="Enter email"/>
+              </div>
+              <div>
+                  <input type="password" v-model="password" placeholder="Password"/>
+              </div>
+              <div>
+                  <button v-on:click="login" class="login">Login</button>
+              </div>
+        </div>
+        <div class="background">
+        </div>
+    </div>
 </template>
 
 <script>
+import navBarLogin from '../components/NavBarLogin.vue';
+import loginMessage from '../messages/LoginMessages.json';
+
 export default {
     name: 'login',
     components: {
-
+        navBarLogin
     },
     data: function () {
       return {
         message: '',
-        password: ''
+        password: '',
+        loginMessage: loginMessage
       }
     },
     methods: {
@@ -36,10 +48,21 @@ export default {
 </script>
 
 <style scoped>
-  #full {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
+    .background {
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        background: #0C1012;
+    }
+
+    #full {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
