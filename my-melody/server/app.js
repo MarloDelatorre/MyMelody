d7d38cf255b7712b668a60fe943922724db7bfa6
Marlo Delatorre <delatorm@purdue.edu>
2018-02-22 19:32:13 -0500

    Fixed routing maybe?

--- my-melody/server/app.js
+++ my-melody/server/app.js
@@ -22,17 +22,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(history({
   disableDotRule: true,
   verbose: true
 }));
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route Registration
 app.use('/api/users', users);
 app.use('/api/spotify', spotify);
 app.use('/api/posts', posts)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
