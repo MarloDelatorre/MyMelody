77baa37b11ef34dc46006df6f72afe337021629e
Pranav Shekar <pshekar@purdue.edu>
2018-02-24 15:22:42 -0600

    took out dist in filepath

--- my-melody/server/app.js
+++ my-melody/server/app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route Registration
 app.use('/api/users', users);
@@ -30,7 +30,7 @@ app.use('/api/spotify', spotify);
 app.use('/api/posts', posts)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // catch 404 and forward to error handler
